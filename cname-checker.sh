a96ec34b3675c3b0237eae17210174e65e788fa8
Moises Bueso <me_bueso@hotmail.com>
2022-10-27 11:02:23 -0600

    adding commands for digging CNAMEs

--- cname-checker.sh
+++ cname-checker.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-#Script to check cname  puppet master from a server
-#Script Name: puppet_checker.sh
+#Script to check cname on Linux Servers
+#Script Name: cname-checker.sh
 #Author: Zenitram
+#Check DNS records for a l;ist of servers
+#Requires at least 1 server to validate records
 
-#Sends a command to a list of servers
-#Requires 1 ip/host per line in the server list and the command to be sent
-#For example 
+#Requirements
+user=mbueso
+server=$1
 
-#server list file name
+#DIG commands with cname option
+cmd1="dig -t CNAME /$1 @172.16.7.126"; #Toronto Linux
+cmd2="dig -t CNAME /$1 @172.20.7.126"; #Vancouver Linux
+cmd3="dig -t CNAME /$1 @172.16.238.101"; #Toronto Windows
+cmd4="dig -t CNAME /$1 @172.16.238.110"; #Vancouver Windows
+
+#Greetings
+echo "Hi $user"
+echo "Today is $date"
